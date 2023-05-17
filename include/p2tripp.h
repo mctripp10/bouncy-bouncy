e1c308dda71b6d7262a47fe87a56af9a3f93ca7f
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2023-05-17 12:03:52 -0400

    Add files via upload

---
+++ include/p2tripp.h
@@ -0,0 +1,16 @@
+#ifndef P2TRIPP_H
+#define P2TRIPP_H
+
+
+class p2tripp
+{
+    public:
+        p2tripp();
+        virtual ~p2tripp();
+
+    protected:
+
+    private:
+};
+
+#endif // P2TRIPP_H
